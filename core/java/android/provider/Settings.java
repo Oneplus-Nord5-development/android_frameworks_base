7ba5c846231c00fc1c3f792bce126f45b85bbacd
Pranav Vashi <neobuddy89@gmail.com>
2017-07-19 23:46:45 +0530

    SystemUI: Allow using 4G icon instead of LTE [1/2]
    [someone5678] Adapt to SystemUI mobile connections pipeline
    shuttercat: adapt to A16 and A16QPR1 kairos changes
    
    Co-authored-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6746,6 +6746,12 @@ public final class Settings {
         public static final String QS_TILE_SHAPE = "qs_tile_shape";
 
         /**
+         * Whether to display 4G icon instead LTE
+         * @hide
+         */
+        public static final String SHOW_FOURG_ICON = "show_fourg_icon";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
